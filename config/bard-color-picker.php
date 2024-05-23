d15382216c8a8678eba0ee9689d19fc8bed280a5
Lars Heidkämper <heidkaemper@new-data-services.de>
2024-05-23 19:30:47 +0000

    prepare new config

--- config/bard-color-picker.php
+++ config/bard-color-picker.php
@@ -4,53 +4,27 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Pickr theme
+    | Color Swatches
     |--------------------------------------------------------------------------
     |
-    | Set the pickr theme
-    | https://simonwep.github.io/pickr/
-    | Supported: "classic", "monolith", "nano"
+    | ..
     |
     */
 
-    'theme' => 'nano',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Main components
-    |--------------------------------------------------------------------------
-    */
-
-    'components' => [
-        'preview' => true, // comparison between old and new color
-        'opacity' => true, // opacity slider
-        'hue'     => true, // hue slider
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Pickr swatches (recommended colors)
-    |--------------------------------------------------------------------------
-    */
-
-    'recommended' => [
+    'swatches' => [
         //'#FF269E',
         //'#01D7B0',
     ],
 
     /*
     |--------------------------------------------------------------------------
-    | Input / output options
+    | Allow any Color
     |--------------------------------------------------------------------------
+    |
+    | ..
+    |
     */
 
-    'interactions' => [
-        'hex'   => false,
-        'rgba'  => false,
-        'hsla'  => false,
-        'hsva'  => false,
-        'cmyk'  => false,
-        'input' => false,
-    ],
+    'allow_any' => true,
 
 ];
