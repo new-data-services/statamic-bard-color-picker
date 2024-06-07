150120aa979dda0a8f7a16ac161e58913f866e68
Lars Heidkämper <heidkaemper@new-data-services.de>
2024-06-07 15:49:54 +0000

    inherit color swatch

--- resources/js/components/ColorPanel.vue
+++ resources/js/components/ColorPanel.vue
@@ -15,9 +15,21 @@
             <div class="ndx-text-color">
                 <div v-if="colorPickerConfig.swatches.length" class="ndx-text-color-swatches">
                     <button
+                        v-if="! colorPickerConfig.allow_any"
+                        v-tooltip="__('Remove')"
+                        :aria-label="__('Remove')"
+                        @click="() => customColorFromSwatch(null)"
+                        class="ndx-text-color-swatch-reset"
+                    >
+                        <svg v-if="swatch === customColor" width="12" height="12" viewBox="0 0 12 12" fill="#fff">
+                            <path d="M11.1897 3.79018C11.1897 3.96875 11.1272 4.12054 11.0022 4.24554L6.15402 9.09375L5.2433 10.0045C5.1183 10.1295 4.96652 10.192 4.78795 10.192C4.60938 10.192 4.45759 10.1295 4.33259 10.0045L3.42188 9.09375L0.997768 6.66964C0.872768 6.54464 0.810268 6.39286 0.810268 6.21429C0.810268 6.03571 0.872768 5.88393 0.997768 5.75893L1.90848 4.84821C2.03348 4.72321 2.18527 4.66071 2.36384 4.66071C2.54241 4.66071 2.6942 4.72321 2.8192 4.84821L4.78795 6.82366L9.1808 2.42411C9.3058 2.29911 9.45759 2.23661 9.63616 2.23661C9.81473 2.23661 9.96652 2.29911 10.0915 2.42411L11.0022 3.33482C11.1272 3.45982 11.1897 3.61161 11.1897 3.79018Z"/>
+                        </svg>
+                    </button>
+
+                    <button
                         v-for="swatch in colorPickerConfig.swatches"
                         :key="swatch"
-                        @click="() => customColor = swatch"
+                        @click="() => customColorFromSwatch(swatch)"
                         :style="{ 'background-color': swatch }"
                     >
                         <svg v-if="swatch === customColor" width="12" height="12" viewBox="0 0 12 12" fill="#fff">
@@ -35,13 +47,13 @@
                             type="color"
                             :value="customColor || '#ffffff'"
                             :data-has-placeholder="! customColor"
-                            @input="event => customColor = event.target.value"
+                            @input="event => customColorFromPicker(event.target.value)"
                         />
 
                         <button
                             :aria-label="__('Remove')"
                             :disabled="! customColor"
-                            @click="() => customColor = null"
+                            @click="() => customColorFromPicker(null)"
                             class="ndx-text-color-reset"
                         >×</button>
                     </div>
@@ -74,10 +86,20 @@
         computed: {
             currentTextColor() {
                 return this.editor.getAttributes('textColor')?.color
-            }
+            },
         },
 
         methods: {
+            customColorFromSwatch(color) {
+                this.customColor = color
+
+                if (! this.colorPickerConfig.allow_any) {
+                    this.setTextColor()
+                }
+            },
+            customColorFromPicker(color) {
+                this.customColor = color
+            },
             setTextColor() {
                 if (this.editor.state.selection.empty) {
                     this.editor.commands.extendMarkRange('textColor')
@@ -86,7 +108,7 @@
                 this.editor.commands.setTextColor({ color: this.customColor })
 
                 this.$refs.popover.close()
-            }
+            },
         },
 
         mounted() {
