93f5783320b71779cd606f7193931e63e72a884d
Lars Heidkämper <heidkaemper@new-data-services.de>
2025-08-15 22:40:09 +0200

    update tests

--- tests/Feature/AssetsTest.php
+++ tests/Feature/AssetsTest.php
@@ -3,16 +3,10 @@
 use Illuminate\Http\Request;
 use Statamic\Statamic;
 
-it('adds necessary js assets', function () {
-    $availableScripts = Statamic::availableScripts(Request::create('/'));
+it('adds necessary assets', function () {
+    $availableVites = Statamic::availableVites(Request::create('/'));
 
-    expect($availableScripts['statamic-bard-color-picker'])
-        ->toContain('addon.js?v=1.0.0');
-});
-
-it('adds necessary css assets', function () {
-    $availableStyles = Statamic::availableStyles(Request::create('/'));
-
-    expect($availableStyles['statamic-bard-color-picker'])
-        ->toContain('addon.css?v=1.0.0');
+    expect($availableVites['statamic-bard-color-picker']['input'])
+        ->toContain('resources/js/addon.js')
+        ->toContain('resources/css/addon.css');
 });
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,49 +2,10 @@
 
 namespace Ndx\BardColorPicker\Tests;
 
-use Orchestra\Testbench\TestCase as OrchestraTestCase;
-use Statamic\Extend\Manifest;
-use Statamic\Statamic;
+use Ndx\BardColorPicker\ServiceProvider;
+use Statamic\Testing\AddonTestCase;
 
-class TestCase extends OrchestraTestCase
+class TestCase extends AddonTestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->withoutVite();
-    }
-
-    protected function getPackageProviders($app)
-    {
-        return [
-            \Statamic\Providers\StatamicServiceProvider::class,
-            \Ndx\BardColorPicker\ServiceProvider::class,
-        ];
-    }
-
-    protected function resolveApplicationConfiguration($app)
-    {
-        parent::resolveApplicationConfiguration($app);
-
-        $app['config']->set('statamic.users.repository', 'file');
-    }
-
-    protected function getPackageAliases($app)
-    {
-        return ['Statamic' => Statamic::class];
-    }
-
-    protected function getEnvironmentSetUp($app)
-    {
-        parent::getEnvironmentSetUp($app);
-
-        $app->make(Manifest::class)->manifest = [
-            'ndx/statamic-bard-color-picker' => [
-                'id'        => 'ndx/statamic-bard-color-picker',
-                'namespace' => 'Ndx\\BardColorPicker',
-                'version'   => '1.0.0',
-            ],
-        ];
-    }
+    protected string $addonServiceProvider = ServiceProvider::class;
 }
