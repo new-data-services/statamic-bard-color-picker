da57c287c703645d131207e27e835a440a241bec
Lars Heidkämper <heidkaemper@new-data-services.de>
2025-08-17 11:56:14 +0200

    update vite, use postcss to resolve css nesting

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+export default {
+    plugins: {
+        'postcss-discard-comments': {},
+        'postcss-nesting': {},
+    },
+}
--- vite.config.js
+++ vite.config.js
@@ -14,4 +14,5 @@ export default defineConfig({
         }),
         vue(),
     ],
+    css: { postcss: __dirname },
 })
