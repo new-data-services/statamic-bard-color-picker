e88476d3d21eb8a5dab92db11309807198988fab
Lars Heidkämper <heidkaemper@new-data-services.de>
2026-05-27 11:54:43 +0200

    update html parsing for the textcolor mark

--- src/Marks/TextColor.php
+++ src/Marks/TextColor.php
@@ -4,17 +4,48 @@ namespace Ndx\BardColorPicker\Marks;
 
 use Tiptap\Core\Mark;
 use Tiptap\Utils\HTML;
+use Tiptap\Utils\InlineStyle;
 
 class TextColor extends Mark
 {
     public static $name = 'textColor';
 
-    public function renderHTML($mark, $HTMLAttributes = [])
+    public function addAttributes()
     {
         return [
-            'span',
-            HTML::mergeAttributes(['style' => "color: {$mark->attrs->color};"]),
-            0,
+            'color' => [
+                'parseHTML' => function ($DOMNode) {
+                    $attribute = InlineStyle::getAttribute($DOMNode, 'color');
+
+                    if ($attribute === null) {
+                        return null;
+                    }
+
+                    return preg_replace('/[\'"]+/', '', $attribute);
+                },
+                'renderHTML' => function ($attributes) {
+                    if (! $attributes?->color) {
+                        return null;
+                    }
+
+                    return ['style' => "color: {$attributes->color}"];
+                },
+            ],
         ];
+    }
+
+    public function parseHTML()
+    {
+        return [
+            [
+                'tag'      => 'span',
+                'getAttrs' => fn ($DOMNode) => InlineStyle::getAttribute($DOMNode, 'color') ? null : false,
+            ],
+        ];
+    }
+
+    public function renderHTML($mark, $HTMLAttributes = [])
+    {
+        return ['span', HTML::mergeAttributes($HTMLAttributes), 0];
     }
 }
--- tests/Feature/TextColorMarkTest.php
+++ tests/Feature/TextColorMarkTest.php
@@ -2,25 +2,62 @@
 
 use Statamic\Fieldtypes\Bard;
 
-it('renders text color marks', function () {
-    $content = [
-        [
-            'type'    => 'paragraph',
-            'content' => [
-                [
-                    'type'  => 'text',
-                    'marks' => [[
-                        'type'  => 'textColor',
-                        'attrs' => ['color' => '#01D7B0'],
-                    ]],
-                    'text' => 'And the world will be as one',
+describe('rendering', function () {
+    it('renders text color marks', function () {
+        $content = [
+            [
+                'type'    => 'paragraph',
+                'content' => [
+                    [
+                        'type'  => 'text',
+                        'marks' => [[
+                            'type'  => 'textColor',
+                            'attrs' => ['color' => '#01D7B0'],
+                        ]],
+                        'text' => 'And the world will be as one',
+                    ],
                 ],
             ],
-        ],
-    ];
+        ];
 
-    $this->assertEquals(
-        (new Bard)->augment($content),
-        '<p><span style="color: #01D7B0;">And the world will be as one</span></p>'
-    );
+        expect((new Bard)->augment($content))
+            ->toBe('<p><span style="color: #01D7B0;">And the world will be as one</span></p>');
+    });
+});
+
+describe('parsing', function () {
+    it('parses text color marks from html', function () {
+        $html = '<p><span style="color: #01D7B0;">And the world will be as one</span></p>';
+
+        $value = (new Bard)->preProcess($html);
+
+        $textNode = $value[0]['content'][0];
+
+        expect($textNode['text'])->toBe('And the world will be as one');
+        expect($textNode['marks'])->toEqual([[
+            'type'  => 'textColor',
+            'attrs' => ['color' => '#01D7B0'],
+        ]]);
+    });
+
+    it('strips quotes from parsed color values', function () {
+        $html = '<p><span style="color: \'#01D7B0\';">Quoted color</span></p>';
+
+        $value = (new Bard)->preProcess($html);
+
+        $textNode = $value[0]['content'][0];
+
+        expect($textNode['marks'][0]['attrs']['color'])->toBe('#01D7B0');
+    });
+
+    it('does not parse spans without color style as text color marks', function () {
+        $html = '<p><span class="highlight">No color here</span></p>';
+
+        $value = (new Bard)->preProcess($html);
+
+        $textNode = $value[0]['content'][0];
+
+        expect($textNode['text'])->toBe('No color here');
+        expect($textNode['marks'] ?? null)->toBeNull();
+    });
 });
